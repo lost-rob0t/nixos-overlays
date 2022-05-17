d4c767fde7e137476a61155ecc1c465c5d6b3cfa
N545PY <nsaspy@airmail.cc>
2022-05-17 05:05:16 -0400

    again i was wrong, i should read the docs

--- pkgs/puffer/default.nix
+++ pkgs/puffer/default.nix
@@ -1,4 +1,4 @@
-{ lib, nimPackages, fetchurl }:
+{ lib, stdenv, nimPackages, fetchurl }:
 
 let
   cligen = nimPackages.buildNimPackage rec {
@@ -12,8 +12,7 @@ let
 
 
   };
-in
-nimPackages.buildNimPackage rec {
+puffer = nimPackages.buildNimPackage rec {
   name = "puffer";
   rev = "d0c7d6a9a9e48e31eb24807f8a24118bc19c031c";
 
@@ -26,11 +25,16 @@ nimPackages.buildNimPackage rec {
   buildInputs = with nimPackages; [
     cligen
   ];
+};
 
-  meta = with lib; {
+in
+  stdenv.mkDerivation rec {
+    pname = "puffer";
+    buildInputs = [ puffer ];
+    meta = with lib; {
     description = "count puffs";
     homepage = "https://github.com/lost-rob0t/puffer.ni,";
     license = licenses.mit;
     maintainers = [ "nsaspy" ];
-  };
-}
+    };
+  }
